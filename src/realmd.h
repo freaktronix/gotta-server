7c2cf93678f10614846690130b9edbd9260a98db
crito <crito@fnordpipe.org>
2018-01-23 14:13:01 +0100

    introduce realmd.h

---
+++ src/realmd.h
@@ -0,0 +1,28 @@
+#ifndef REALMD_H
+#define REALMD_H
+
+#define REALMD_VERSION "0.0.1"
+
+#define REALMD_PORT "17420"
+#define REALMD_SOCKET_QUEUE 128
+
+typedef struct realmd_peer_t realmd_peer_t;
+
+typedef struct {
+	int sock;
+	realmd_peer_t *peers;
+} realmd_listener_t;
+
+struct realmd_peer_t {
+	int sock;
+	struct sockaddr_in *client;
+	int addrlen;
+	realmd_peer_t *next;
+};
+
+void realmd_new_peer(realmd_peer_t *peer);
+void realmd_loop(realmd_listener_t *gotta);
+void realmd_new_peer(realmd_peer_t *peer);
+int realmd_start_listen(realmd_listener_t *gotta);
+
+#endif
